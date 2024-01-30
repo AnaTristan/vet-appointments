f5179706d416a9b01f885527e47b8fda4615895a
Ana Tristan <programacion1@grupoenticonfio.com>
2024-01-30 13:26:41 -0600

    adding clear form and Form props

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-
-import Header from "./components/Header"
-import AppForm from "./components/Form"
-import PatientsList from "./components/PatientsList"
+import { useState } from "react";
+import Header from "./components/Header";
+import AppForm from "./components/Form";
+import PatientsList from "./components/PatientsList";
 
 function App() {
+  const [patients, setPatients] = useState([]);
 
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <AppForm />
+        <AppForm 
+          patients={patients}
+          setPatients={setPatients} 
+        />
         <PatientsList />
       </div>
     </div>
-  )
-
+  );
 }
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,108 +1,157 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-function AppForm() {
+function AppForm({ patients, setPatients }) {
+  const [name, setName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [email, setEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [sin, setSin] = useState("");
 
-    const [name, setName] = useState('');
-    const [owner, setOwner] = useState('');
-    const [email, setEmail] = useState('');
-    const [date, setDate] = useState('');
-    const [sin, setSin] = useState('');
+  const [error, setError] = useState(false);
 
-    const [error, setError] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // form validation
+    if ([name, owner, email, date, sin].includes("")) {
+      setError(true);
+      return;
+    } setError(false);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // form validation
-        if ([name, owner, email, date, sin].includes('')) {
-            setError(true);
-        }
-        else{
-            setError(false);
-        }
+    //patients object
 
-    }
+    const patientsObject = {
+      name,
+      owner,
+      email,
+      date,
+      sin,
+    };
+    setPatients([...patients, patientsObject]);
 
-    return (
+    // Clear form
+    setName("");
+    setOwner("");
+    setEmail("");
+    setDate("");
+    setSin("");
+  };
 
-        <div className="md:w-1/2 lg:w-3/5 md:h-screen mx-5">
-            <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen mx-5">
+      <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
 
-            <p className="text-lg mt-5 text-center mb-10">
-                Anade pacientes y <span className="text-indigo-600 font-bold">administralos</span>
+      <p className="text-lg mt-5 text-center mb-10">
+        Anade pacientes y{" "}
+        <span className="text-indigo-600 font-bold">administralos</span>
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+        {error && (
+          <div>
+            <p className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3">
+              Todos los campos son obligatorios
             </p>
-
-            <form
-                onSubmit={handleSubmit}
-                className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
-            >
-                {error && (
-                    <div>
-                        <p className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3">Todos los campos son obligatorios</p>
-                    </div>
-                )}
-                <div className="mb-5">
-                    <label htmlFor="pet" className="block text-gray-700 font-bold uppercase">Nombre Mascota</label>
-                    <input
-                        id="pet"
-                        type="text"
-                        placeholder="Nombre de la mascota"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={name}
-                        onChange={(e) => { setName(e.target.value) }}
-                    />
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="owner" className="block text-gray-700 font-bold uppercase">Nombre Propietario</label>
-                    <input
-                        id="owner"
-                        type="text"
-                        placeholder="Nombre propietario"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={owner}
-                        onChange={(e) => { setOwner(e.target.value) }}
-                    />
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="Email" className="block text-gray-700 font-bold uppercase">Email</label>
-                    <input
-                        id="Email"
-                        type="email"
-                        placeholder="Email"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={email}
-                        onChange={(e) => { setEmail(e.target.value) }}
-                    />
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="date" className="block text-gray-700 font-bold uppercase">Alta</label>
-                    <input
-                        id="date"
-                        type="date"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        value={date}
-                        onChange={(e) => { setDate(e.target.value) }}
-                    />
-                </div>
-                <div className="mb-5">
-                    <label htmlFor="date" className="block text-gray-700 font-bold uppercase">Alta</label>
-                    <textarea
-                        id="sintoms"
-                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-                        placeholder="Describe los sintomas"
-                        value={sin}
-                        onChange={(e) => { setSin(e.target.value) }}
-                    />
-                </div>
-                <input
-                    type="submit"
-                    className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-                    value="Agregar paciente"
-                />
-            </form>
+          </div>
+        )}
+        <div className="mb-5">
+          <label
+            htmlFor="pet"
+            className="block text-gray-700 font-bold uppercase"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="pet"
+            type="text"
+            placeholder="Nombre de la mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
         </div>
-
-    )
+        <div className="mb-5">
+          <label
+            htmlFor="owner"
+            className="block text-gray-700 font-bold uppercase"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            id="owner"
+            type="text"
+            placeholder="Nombre propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={owner}
+            onChange={(e) => {
+              setOwner(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="Email"
+            className="block text-gray-700 font-bold uppercase"
+          >
+            Email
+          </label>
+          <input
+            id="Email"
+            type="email"
+            placeholder="Email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="date"
+            className="block text-gray-700 font-bold uppercase"
+          >
+            Alta
+          </label>
+          <input
+            id="date"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="date"
+            className="block text-gray-700 font-bold uppercase"
+          >
+            Sintomas
+          </label>
+          <textarea
+            id="sintoms"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los sintomas"
+            value={sin}
+            onChange={(e) => {
+              setSin(e.target.value);
+            }}
+          />
+        </div>
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value="Agregar paciente"
+        />
+      </form>
+    </div>
+  );
 }
 
-export default AppForm
-
+export default AppForm;
