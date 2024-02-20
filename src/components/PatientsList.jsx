25e3c7dc38ebaae9854a6770004588d2d0ffdca5
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-02-19 23:22:11 -0600

    fixing merge issues

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -1,37 +1,14 @@
 import { useState, useEffect } from "react";
 import Patient from "./Patient";
 
-<<<<<<< HEAD
-const PatientsList = ({ patients,setPatient }) => {
-    return (
-        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-=======
-const PatientsList = ({ patients }) => {
->>>>>>> fbadffdb1f017be5aa06d4b5edf5e2b9b276192a
+
+const PatientsList = ({ patients, setPatient }) => {
 
   useEffect(() => {
-    if(patients.length>0)
-        console.log("new patient");
+    if (patients.length > 0)
+      console.log("new patient");
   }, [patients]);
 
-<<<<<<< HEAD
-                <>
-                    <h2 className="font-black text-3xl text-center">Pacientes</h2>
-                    <p className="text-xl mt-5 mb-10 text-center">
-                        Administra tus {''}
-                        <span className="text-indigo-600 font-bold">pacientes y citas</span>
-                    </p>
-                    {
-                        patients.map(patient => (
-                            <Patient
-                                key={patient.id}
-                                {...patient}                
-                                setPatient={setPatient}
-                            />
-                        ))
-                    }
-                </>
-=======
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
       {patients && patients.length ? (
@@ -57,6 +34,5 @@ const PatientsList = ({ patients }) => {
     </div>
   );
 };
->>>>>>> fbadffdb1f017be5aa06d4b5edf5e2b9b276192a
 
 export default PatientsList;
